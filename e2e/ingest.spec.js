4512994aa03dbea37def3d5ff99b495bc15a1ddc
Marco La Rosa <m@lr.id.au>
2018-01-28 08:39:23 +1100

    add e2e hierarchy

---
+++ e2e/ingest.spec.js
@@ -0,0 +1,7 @@
+const chakram   = require('chakram');
+const expect    = chakram.expect;
+
+describe('verify xml import route working as expected', () => {
+    it('should get a successful response from the import endpoint', () => {})
+    it('should get an error response from the import endpoint', () => {})
+})
