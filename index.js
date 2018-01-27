a6ae1bb24f235fbb883edd6a07026fef43c3eb09
Marco La Rosa <m@lr.id.au>
2018-01-28 10:34:39 +1100

    refactor so that we can export the app if required

--- index.js
+++ index.js
@@ -4,25 +4,33 @@ const express = require('express');
 const models = require('./src/models');
 const routeHandlers = require('./src/routeHandlers');
 
-models.sequelize
-    .sync()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-        initialiseApplication();
-    })
-    .catch(e => {
-        console.log(e);
-    });
+return createApp().then(app => {
+    return app.listen(3000, () =>
+        console.log('Markr micro-service listening on port 3000!')
+    );
+    return app;
+});
 
-function initialiseApplication() {
-    const app = express();
+function createApp() {
+    return models.sequelize
+        .sync()
+        .then(() => {
+            console.log('DB connection has been established successfully.');
+            const app = express();
+            wireUpRoutes(app);
+            return app;
+        })
+        .catch(e => {
+            // console.log(e);
+        });
+}
+
+function wireUpRoutes(app) {
     app.post('/import', routeHandlers.post.ingest.processMarkedData);
     app.get(
         '/results/:test-id/aggregate',
         routeHandlers.get.results.getAggregateResults
     );
-
-    app.listen(3000, () =>
-        console.log('Markr micro-service listening on port 3000!')
-    );
 }
+
+module.exports = createApp;
