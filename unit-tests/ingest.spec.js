5c2592f08881c616a96b492ba5434e877fe9bc44
Marco La Rosa <m@lr.id.au>
2018-01-28 08:29:59 +1100

    refactor; test updating the same record

--- unit-tests/ingest.spec.js
+++ unit-tests/ingest.spec.js
@@ -2,73 +2,23 @@
 
 const chakram = require('chakram');
 const expect = chakram.expect;
+const models = require('src/models');
 
 const {
     processMarkedDataCtrl,
     convertToJS,
+	createOrUpdateResultRecord,
     extractMcqTestResults,
     extractMcqTestResult
 } = require('src/controllers/ingest');
 
-const wellFormedResult = `
-	<mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
-		<first-name>KJ</first-name>
-		<last-name>Alysander</last-name>
-		<student-number>002299</student-number>
-		<test-id>9863</test-id>
-		<answer question="0" marks-available="1" marks-awarded="1">D</answer>
-		<summary-marks available="1" obtained="1" />
-	</mcq-test-result>
-`;
-
-const notWellFormedResult = `
-	<mcq-testing-result scanned-on="2017-12-04T12:12:10+11:00">
-	</mcq-testing-result>
-`;
-
-const resultWithDataMissing = `
-	<mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
-		<first-name>KJ</first-name>
-		<last-name>Alysander</last-name>
-		<student-number>002299</student-number>
-	</mcq-test-result>
-`;
-
-const wellFormedDocument = `
-<mcq-test-results>
-	<mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
-		<first-name>KJ</first-name>
-		<last-name>Alysander</last-name>
-		<student-number>002299</student-number>
-		<test-id>9863</test-id>
-		<answer question="0" marks-available="1" marks-awarded="1">D</answer>
-		<answer question="1" marks-available="1" marks-awarded="1">D</answer>
-		<answer question="2" marks-available="1" marks-awarded="1">D</answer>
-		<answer question="3" marks-available="1" marks-awarded="0">C</answer>
-		<answer question="4" marks-available="1" marks-awarded="1">B</answer>
-		<answer question="5" marks-available="1" marks-awarded="0">D</answer>
-		<answer question="6" marks-available="1" marks-awarded="0">A</answer>
-		<answer question="7" marks-available="1" marks-awarded="1">A</answer>
-		<answer question="8" marks-available="1" marks-awarded="1">B</answer>
-		<answer question="9" marks-available="1" marks-awarded="1">D</answer>
-		<answer question="10" marks-available="1" marks-awarded="1">A</answer>
-		<answer question="11" marks-available="1" marks-awarded="1">B</answer>
-		<answer question="12" marks-available="1" marks-awarded="0">A</answer>
-		<answer question="13" marks-available="1" marks-awarded="0">B</answer>
-		<answer question="14" marks-available="1" marks-awarded="1">B</answer>
-		<answer question="15" marks-available="1" marks-awarded="1">A</answer>
-		<answer question="16" marks-available="1" marks-awarded="1">C</answer>
-		<answer question="17" marks-available="1" marks-awarded="0">B</answer>
-		<answer question="18" marks-available="1" marks-awarded="1">A</answer>
-		<answer question="19" marks-available="1" marks-awarded="0">B</answer>
-		<summary-marks available="20" obtained="13" />
-	</mcq-test-result>
-</mcq-test-results>
-`;
-
 describe('verify xml import route working as expected', () => {
+	afterEach(() => {
+		return models.result.destroy({ where: {}});
+	})
+
     it('should be able to process a well formed result', () => {
-        const doc = convertToJS(wellFormedResult);
+        const doc = convertToJS(getTestData().wellFormedResult);
         let result = extractMcqTestResult(doc.elements[0]);
 
         expect(result.firstName).to.equal('KJ');
@@ -77,18 +27,124 @@ describe('verify xml import route working as expected', () => {
         expect(result.testId).to.equal('9863');
         expect(result.summaryMarksObtained).to.equal(1);
     });
-    it('should fail on a result with an incorrect root element', () => {
-        const doc = convertToJS(notWellFormedResult);
+    it('should ignore a result with an incorrect root element', () => {
+        const doc = convertToJS(getTestData().notWellFormedResult);
         let result = extractMcqTestResult(doc.elements[0]);
         expect(result).to.equal(undefined);
     });
-    it('should fail on a result with missing fields', () => {
-        const doc = convertToJS(resultWithDataMissing);
+    it('should report a result with missing fields', () => {
+        const doc = convertToJS(getTestData().resultWithDataMissing);
         let result = extractMcqTestResult(doc.elements[0]);
         expect(result).to.equal(null);
     });
     it('should be able to process a well formed document', () => {
-        const results = processMarkedDataCtrl(wellFormedDocument);
+        const results = processMarkedDataCtrl(getTestData().wellFormedDocument);
         expect(results.length).to.equal(1);
     });
+	it('should override a previous lower result', () => {
+		let doc = convertToJS(getTestData().wellFormedResult);
+		let result = extractMcqTestResult(doc.elements[0]);
+		return createOrUpdateResultRecord(result)
+		.then(result => {
+			expect(result.summaryMarksObtained).to.equal(1);
+			return Promise.resolve();
+		})
+		.then(() => {
+			doc = convertToJS(getTestData().duplicateWellFormedResult);
+			result = extractMcqTestResult(doc.elements[0]);
+			return createOrUpdateResultRecord(result);
+		})
+		.then(result => {
+			expect(result.summaryMarksObtained).to.equal(2);
+		});
+	})
+	it('should not override a previous higher result', () => {
+		let doc = convertToJS(getTestData().duplicateWellFormedResult);
+		let result = extractMcqTestResult(doc.elements[0]);
+		return createOrUpdateResultRecord(result)
+		.then(result => {
+			expect(result.summaryMarksObtained).to.equal(2);
+			return Promise.resolve();
+		})
+		.then(() => {
+			doc = convertToJS(getTestData().wellFormedResult);
+			result = extractMcqTestResult(doc.elements[0]);
+			return createOrUpdateResultRecord(result);
+		})
+		.then(result => {
+			expect(result.summaryMarksObtained).to.equal(2);
+		});
+	})
+
 });
+
+
+function getTestData() {
+
+    return {
+        wellFormedResult: `
+            <mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
+                <first-name>KJ</first-name>
+                <last-name>Alysander</last-name>
+                <student-number>002299</student-number>
+                <test-id>9863</test-id>
+                <answer question="0" marks-available="1" marks-awarded="1">D</answer>
+                <answer question="0" marks-available="1" marks-awarded="0">D</answer>
+                <summary-marks available="2" obtained="1" />
+            </mcq-test-result>
+        `,
+		duplicateWellFormedResult: `
+			<mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
+				<first-name>KJ</first-name>
+				<last-name>Alysander</last-name>
+				<student-number>002299</student-number>
+				<test-id>9863</test-id>
+				<answer question="0" marks-available="1" marks-awarded="1">D</answer>
+				<answer question="0" marks-available="1" marks-awarded="1">D</answer>
+				<summary-marks available="2" obtained="2" />
+			</mcq-test-result>
+		`,
+		notWellFormedResult: `
+			<mcq-testing-result scanned-on="2017-12-04T12:12:10+11:00">
+			</mcq-testing-result>
+		`,
+		resultWithDataMissing: `
+			<mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
+				<first-name>KJ</first-name>
+				<last-name>Alysander</last-name>
+				<student-number>002299</student-number>
+			</mcq-test-result>
+		`,
+		wellFormedDocument: `
+			<mcq-test-results>
+				<mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
+					<first-name>KJ</first-name>
+					<last-name>Alysander</last-name>
+					<student-number>002299</student-number>
+					<test-id>9863</test-id>
+					<answer question="0" marks-available="1" marks-awarded="1">D</answer>
+					<answer question="1" marks-available="1" marks-awarded="1">D</answer>
+					<answer question="2" marks-available="1" marks-awarded="1">D</answer>
+					<answer question="3" marks-available="1" marks-awarded="0">C</answer>
+					<answer question="4" marks-available="1" marks-awarded="1">B</answer>
+					<answer question="5" marks-available="1" marks-awarded="0">D</answer>
+					<answer question="6" marks-available="1" marks-awarded="0">A</answer>
+					<answer question="7" marks-available="1" marks-awarded="1">A</answer>
+					<answer question="8" marks-available="1" marks-awarded="1">B</answer>
+					<answer question="9" marks-available="1" marks-awarded="1">D</answer>
+					<answer question="10" marks-available="1" marks-awarded="1">A</answer>
+					<answer question="11" marks-available="1" marks-awarded="1">B</answer>
+					<answer question="12" marks-available="1" marks-awarded="0">A</answer>
+					<answer question="13" marks-available="1" marks-awarded="0">B</answer>
+					<answer question="14" marks-available="1" marks-awarded="1">B</answer>
+					<answer question="15" marks-available="1" marks-awarded="1">A</answer>
+					<answer question="16" marks-available="1" marks-awarded="1">C</answer>
+					<answer question="17" marks-available="1" marks-awarded="0">B</answer>
+					<answer question="18" marks-available="1" marks-awarded="1">A</answer>
+					<answer question="19" marks-available="1" marks-awarded="0">B</answer>
+					<summary-marks available="20" obtained="13" />
+				</mcq-test-result>
+			</mcq-test-results>
+		`
+    }
+}
