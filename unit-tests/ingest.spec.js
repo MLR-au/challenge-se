37cdc4b9774be049b6e42bc265b737c80d45c22a
Marco La Rosa <m@lr.id.au>
2018-01-27 21:32:13 +1100

    Scaffold out unit testing hierarchy
    commit 3ccd51201d1e3fc677f88e818a992d5d483cecdf
    Author: Marco La Rosa <m@lr.id.au>
    Date:   Sat Jan 27 21:31:56 2018 +1100
    
        alignment
    
    commit b0a018a43e3efbcb0ebc7f4d53e1e1ff36e056b2
    Author: Marco La Rosa <m@lr.id.au>
    Date:   Sat Jan 27 21:18:37 2018 +1100
    
        beginning to implement test suite

--- unit-tests/ingest.spec.js
+++ unit-tests/ingest.spec.js
@@ -1,8 +1,27 @@
 'use strict';
 
+const chakram = require('chakram');
+const expect = chakram.expect;
+
 const processMarkedDataCtrl = require('src/controllers/ingest')
     .processMarkedDataCtrl;
 
+const wellFormedDocument = `
+<mcq-test-results>
+	<mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
+		<first-name>KJ</first-name>
+		<last-name>Alysander</last-name>
+		<student-number>002299</student-number>
+		<test-id>9863</test-id>
+		<answer question="0" marks-available="1" marks-awarded="1">D</answer>
+		<summary-marks available="1" obtained="1" />
+	</mcq-test-result>
+</mcq-test-results>
+`;
+
 describe('verify xml import route working as expected', () => {
-    it('should accept a well formed xml document', () => {});
+    it('should accept a well formed xml document', () => {
+        // const result = processMarkedDataCtrl(wellFormedDocument);
+        // expect(processMarkedDataCtrl(wellFormedDocument)).to.be('ok');
+    });
 });
